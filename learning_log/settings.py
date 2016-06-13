cd6ce1576fb90c748f3bab87075759766d7e8e4a
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-06-13 00:01:03 -0300

    fix settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -127,4 +127,4 @@ BOOTSTRAP3 = {'include_jquery': True}
 if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
